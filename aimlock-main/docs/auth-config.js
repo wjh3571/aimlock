e74b40ca5e1301c75b79510dbe30d4f2c22c1baa
wjh3571 <junhyuk3571@gmail.com>
2026-06-03 01:20:07 +0900

    Delete aimlock-main directory

--- aimlock-main/docs/auth-config.js
+++
@@ -1,3 +0,0 @@
-window.AIMLOCK_AUTH = {
-  googleClientId: "449954497595-n9nu6uainfts8vnm7h0m96e52p484e5m.apps.googleusercontent.com",
-};
--- aimlock-main/web/baas-config.js
+++
@@ -1,7 +0,0 @@
-window.AIMLOCK_BAAS = {
-  enabled: true,
-  provider: "supabase",
-  supabaseUrl: "https://kkjizgiczlsoexzrqjtt.supabase.co",
-  supabaseAnonKey: "sb_publishable_guWtd7NReN4TN_h2mPHI6w_Fr0UbgMx",
-  table: "crosshairs",
-};
